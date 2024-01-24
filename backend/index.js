728f752cb390adf07c7d44f7b955c842380d38bb
aayushk9 <ayushkokate1@gmail.com>
2024-01-24 14:36:15 +0530

    activate backend

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from backend')
+})
+
+app.listen(3000, (req, res) => {
+    console.log(`Server is listening on port 3000`);
+})
 
 
