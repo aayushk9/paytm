51d8787609aed65186cdcc39ae488a5565cbf56b
Aayush kokate <96991518+aayushk9@users.noreply.github.com>
2024-01-25 17:18:58 +0530

    add routes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,41 @@
 const express = require("express");
 const app = express();
+const { db } = require("./db");
 
+app.use(express.json());
+
+// home route
 app.get('/', (req, res) => {
-    res.send('Hello from backend')
+    res.send(`Paytm`)
+})
+
+// get request for signup page
+app.get('/signup', (req, res) => {
+  res.send('signup')
+})
+
+// post request for signup page
+app.post('/signup', (req, res) => {
+    // signing up
+});
+
+// get request for signin page
+app.get('/signin', (req, res) => {
+    res.send(`signin`)
+})
+
+// post request for signin page 
+app.post('/signin', (req, res) => {
+
+})
+
+// update data
+app.get('/settings', (req, res) => {
+    res.send('update profile')
+})
+
+app.put('/settings', (req, res) => {
+    // perform operation
 })
 
 app.listen(3000, (req, res) => {
