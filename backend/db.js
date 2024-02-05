fd9ea4962ac5dbc302e493d3ec487c0a433d597c
aayushk9 <ayushkokate1@gmail.com>
2024-02-05 17:20:04 +0530

    add schema

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,16 @@
-<<<<<<< HEAD
 require('dotenv').config();
 const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri);
-=======
->>>>>>> cf2a14d7d7e2ccd8e5c729ad528c6667bdf641d2
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: string
+})
+
+const user = new mongoose.model('user', userSchema)
+
+module.exports ={
+    user
+}
 
