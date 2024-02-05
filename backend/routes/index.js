982ebb6d2710ad1536e05c81575f6f0d80ba87e2
aayushk9 <ayushkokate1@gmail.com>
2024-02-05 17:04:41 +0530

    Resolved conflicts

---
+++ backend/routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+const userRouter = require('./user');
+
+
+const router = express.Router();
+
+router.use('/user', userRouter)
+
+router.get('/', (req, res, next) => {
+   console.log('Working');
+})
+
+module.exports = router;
+
+
+
+
+
