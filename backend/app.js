5e223a0b000c3ebcee50466b84cc9b703ca93c77
aayushk9 <ayushkokate1@gmail.com>
2025-03-07 17:35:21 +0530

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
+
+app.get("/", (req, res) => {
+    return res.json({
+        msg: "hello"
+    })
+})
