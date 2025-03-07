6e4a0fecfc3408aa1d2648cf34e9c8f610045523
aayushk9 <ayushkokate1@gmail.com>
2025-03-07 20:45:45 +0530

    add express routing

---
+++ backend/routes/user.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router()
+
+
+router.get("/", (req, res) => {
+    return res.json({
+        msg: "hello"
+    })
+})
+module.exports = router
