26e6a77131b0f15d100aae6b33f3d21da15c7d0c
aayushk9 <ayushkokate1@gmail.com>
2025-03-08 10:28:18 +0530

    add accounts model

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,11 +1,32 @@
 const mongoose = require("mongoose");
+const URI = process.env.MONGO_URI;
 
-const users = new mongoose.Schema({
+mongoose.connect(URI)   
+ .then(() => (console.log(`Database connected`)))
+ .catch((err) => (console.error(err)))
+
+const userSchema = new mongoose.Schema({
     username: String,
     firstName: String,
     lastName: String,
     password: String
 })
 
-const User = mongoose.model('User', users)
-module.exports = User
+const accountSchema = new mongoose.Schema({
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    balance:  {
+       type: Number,
+       required: true
+    }
+})
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema)
+
+module.exports = {
+    Account, User
+}
