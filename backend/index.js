659b4f429f0eb18e700bbc6b6322d710b0bd05e2
aayushk9 <ayushkokate1@gmail.com>
2025-03-08 10:30:39 +0530

    erase

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,13 @@
 require('dotenv').config();
 const express = require("express");
-const { mongoose } = require('mongoose');
 const cors = require("cors");
 const app = express();
 const port = 3000;
-const URI = process.env.MONGO_URI;
 
 app.use(express.json());
 app.use(cors());
 
 const rootRouter = require("./routes/index");
-
-
-mongoose.connect(URI)   
- .then(() => (console.log(`Database connected`)))
- .catch((err) => (console.error(err)))
-
 
 app.use("/api/v", rootRouter);
 
