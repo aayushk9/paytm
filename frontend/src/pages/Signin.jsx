79b874b5b1d7968ad694887aee8b7633b6a22653
aayushk9 <ayushkokate1@gmail.com>
2025-03-11 03:41:25 +0530

    connect login/signup routes to backend

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,7 +1,41 @@
+import { Headers } from '../components/Headers'
+import { InputBox } from '../components/InputBox'
+import { Button } from '../components/Button'
+import { BottomWarning } from '../components/BottomWarning'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
 export function Signin() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate()
+
     return  (
         <div>
-            <p>Signup</p>
+           <Headers content={"payment methods"}/>
+           <InputBox label={"username"} placeholder={"joe"} onChange={(e) => {
+              setUsername(e.target.value)
+           }}/>
+           <InputBox label={"password"} placeholder={"*****"} onChange={(e) => {
+              setPassword(e.target.value)
+           }}/>
+           <Button buttonName={"Login"} onClick={async() => {
+            const res = await axios.post("http://localhost:3000/api/v/user/login", {
+                username,
+                password
+            }, {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+
+            if(res.data.token) {
+                localStorage.setItem("token", res.data.token);
+                navigate("/dashboard")
+            }
+           }}/>
+           <BottomWarning text={"Don't have an account?"} buttonText={"Signup"} to={"/"}/>
         </div>
     )
-}
+}   
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import axios from 'axios'
 import { useNavigate } from "react-router-dom";
+import { Headers } from '../components/Headers'
+import { InputBox } from '../components/InputBox'
+import { Button } from '../components/Button'
+import { BottomWarning } from '../components/BottomWarning'
 
 export function Signup() {
     const [username, setUsername] = useState("");
@@ -9,68 +13,47 @@ export function Signup() {
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
 
-    
-       const handleSubmit = async(e) => {
-           e.preventDefault(); 
-
-          const loginBody = {
-            username,
-            firstName,
-            lastName,
-            password
-          }
-          try {
-          const res = await axios.post("http://localhost:3000/api/v/user/signup", loginBody, {
-            headers: {
-                'Content-type': "application/json"
-            }
-          })
-
-          console.log(res.data);
-
-          if(res.data.token && res.data.BankBalance){
-            // redirect to dashboard 
-             navigate("/dashboard")
-          } else {
-            alert("some error occured")
-          }
-        } catch (err) {
-            console.log(err)
-        }
-       }
     return (
-        <div>
-            <form onClick={handleSubmit}> 
-                <input
-                    placeholder="Enter username"
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-
-                <input
-                  placeholder="Enter firstname"
-                  type = "text"
-                  value={firstName}
-                  onChange={(e) => {setFirstName(e.target.value)}}
-                />
-
-                <input 
-                 placeholder="Enter lastname"
-                 type = "text"
-                 value= {lastName}
-                 onChange={(e) => {setLastName(e.target.value)}}
-                />
-
-                <input 
-                 placeholder="Enter password"
-                 type = "password" 
-                 value={password}
-                 onChange={(e) => {setPassword(e.target.value)}}
-                />
-
-                <button type="submit">Signup</button>
-            </form>
+        <div className="flex flex-col items-center justify-center">
+          <Headers content={"Payments app"}/>
+          <InputBox onChange={(e) => {
+            setUsername(e.target.value)
+          }}  label={"username"} placeholder="ayushdev"/> 
+          <InputBox onChange={(e) => {
+            setFirstName(e.target.value)
+          }} label={"firstname"} placeholder="Aayush"/>
+          <InputBox onChange={(e) => {
+            setLastName(e.target.value)
+          }} label={"lastname"} placeholder="Kokate" />
+          <InputBox onChange={(e) => {
+            setPassword(e.target.value)
+          }} label={"password"} placeholder="******"/>
+          <Button buttonName={"Submit"} onClick={async () => {
+            try {
+             const res = await axios.post("http://localhost:3000/api/v/user/signup", {
+              username,
+              firstName,
+              lastName,
+              password
+             }, {
+              headers: {
+                "Content-Type": "application/json"
+              }
+             }  
+            )
+              if(res.data.token){
+                localStorage.setItem("token", res.data.token)
+                navigate("/dashboard")
+              } else {
+                alert("Oops something went wrong")
+              }
+            } catch(err) {
+              console.log(err)
+              alert("error signing up")   
+            }            
+          }}
+          />
+         <BottomWarning text={"Already have an account"} buttonText={"login"} to={'/login'}/> 
         </div>
     )
 }
