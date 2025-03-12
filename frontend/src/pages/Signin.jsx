4377b17eed590e3c8b2c9cd7a15be2bc2fa6916b
aayushk9 <ayushkokate1@gmail.com>
2025-03-12 13:21:31 +0530

    fix: add conditions

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -11,31 +11,41 @@ export function Signin() {
     const [password, setPassword] = useState("");
     const navigate = useNavigate()
 
-    return  (
+    return (
         <div>
-           <Headers content={"payment methods"}/>
-           <InputBox label={"username"} placeholder={"joe"} onChange={(e) => {
-              setUsername(e.target.value)
-           }}/>
-           <InputBox label={"password"} placeholder={"*****"} onChange={(e) => {
-              setPassword(e.target.value)
-           }}/>
-           <Button buttonName={"Login"} onClick={async() => {
-            const res = await axios.post("http://localhost:3000/api/v/user/login", {
-                username,
-                password
-            }, {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
+            <Headers content={"payment methods"} />
+            <InputBox label={"username"} placeholder={"joe"} onChange={(e) => {
+                setUsername(e.target.value)
+            }} />
+            <InputBox label={"password"} placeholder={"*****"} onChange={(e) => {
+                setPassword(e.target.value)
+            }} />
+            <Button buttonName={"Login"} onClick={async () => {
+                try {
+                    const res = await axios.post("http://localhost:3000/api/v/user/login", {
+                        username,
+                        password
+                    }, {
+                        headers: {
+                            "Content-Type": "application/json"
+                        }
+                    })
 
-            if(res.data.token) {
-                localStorage.setItem("token", res.data.token);
-                navigate("/dashboard")
-            }
-           }}/>
-           <BottomWarning text={"Don't have an account?"} buttonText={"Signup"} to={"/"}/>
+                    if (res.data.token) {
+                        localStorage.setItem("token", res.data.token);
+                        navigate("/dashboard")
+                    } else if (res.data.error === "Invalid input") {
+                        alert("not valid inputs")
+                    } else if(res.data.msg === "wrong password"){
+                        alert("wrong password")  
+                    } else {
+                        alert("internal issue please try later")
+                    }
+                } catch (err) {  
+                    console.error("Signup Error:", err);
+                }
+            }} />   
+            <BottomWarning text={"Don't have an account?"} buttonText={"Signup"} to={"/"} />
         </div>
     )
 }   
