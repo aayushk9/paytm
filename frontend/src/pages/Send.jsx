0ceaa4fda0437a53fa2c34576ac0dc2fd86273ea
aayushk9 <ayushkokate1@gmail.com>
2025-03-12 13:25:44 +0530

    connect to backend

--- frontend/src/pages/Send.jsx
+++ frontend/src/pages/Send.jsx
@@ -1,7 +1,37 @@
+import { Headers } from '../components/Headers'
+import { ProfileIcon } from '../components/ProfileIcon'
+import { InputBox } from '../components/InputBox'
+import { useSearchParams } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react'
+
 export function Send() {
+    const [searchParams] = useSearchParams()
+    const [amount, setAmount] = useState(0);
+    const id = searchParams.get('id');
+    const name = searchParams.get("name")
+
     return  (
         <div>
-            <p>Send money</p>
+             <Headers content={"Send money"} />
+             <ProfileIcon userProfile={name[0].toUpperCase()} sendMoneyTo={name}/>
+            <InputBox label={"Amount (in Rs)"} placeholder={"Enter amount"} onChange={(e) => {
+                setAmount(e.target.value)
+            }}/>
+            <button onClick={ async() => {
+                const res = await axios.post("http://localhost:3000/api/v/accounts/transfer", {
+                    to: id,
+                    amount: amount
+                 }, {
+                    headers: {
+                        Authorization: "Bearer " + localStorage.getItem("token")
+                    }
+                 })
+
+                 if(res.data.success){
+                     alert("transaction successfull")
+                 }
+            }}>Initiate transfer</button>
         </div>
     )
 }
