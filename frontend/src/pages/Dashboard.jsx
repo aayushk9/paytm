1d0a4f9f5caad17851076ab0e6a4481c55478f4f
aayushk9 <ayushkokate1@gmail.com>
2025-03-12 13:27:08 +0530

    feat(page): add dashboard functionality

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,41 @@
-export function Dashboard () {
-    return (
-        <div>
-            <p>Dashboard</p>
-        </div>
-    )
+import { Headers } from '../components/Headers'
+import { Balance } from '../components/Balance'
+import { ProfileIcon } from '../components/ProfileIcon'
+import { User } from '../components/User'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+export function Dashboard() {
+   const [amount, setAmount] = useState("");
+
+   const fetchBalance = async () => { 
+      try {
+         const res = await axios.get("http://localhost:3000/api/v/accounts/balance", {
+            headers: {
+               "Authorization": `Bearer ${localStorage.getItem("token")}`
+            }
+         })
+         console.log(res.data);
+         if (res.data && res.data.balance !== undefined) {
+            setAmount(res.data.balance)
+         } else {
+            console.error("Balance not found in response: " + res.data)
+         }
+      } catch (err) {
+         alert("some error occured");    
+      }
+   }
+   
+   useEffect(() => {
+    fetchBalance()
+   }, [amount])
+
+   return (
+      <div>
+         <Headers content={"Payments app"} />
+         <ProfileIcon text={'Hello'} userProfile={"userprofile"} />
+         <Balance text={"Your balance Rs"} amount={amount} />
+         <User/>
+      </div>
+   )
 }
